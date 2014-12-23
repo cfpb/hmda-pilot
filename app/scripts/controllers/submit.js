0cc8022487c1feb18daacf57d362e84f447f2af0
Justin Stockton <jstockton@devis.com>
2014-12-23 16:38:10 -0700

    Stub out controllers and view for the summary pages that will make up the steps in the wizard

---
+++ app/scripts/controllers/submit.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hmdaPilotApp.controller:SubmitCtrl
+ * @description
+ * # SubmitCtrl
+ * Controller of the hmdaPilotApp
+ */
+module.exports = function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+};
