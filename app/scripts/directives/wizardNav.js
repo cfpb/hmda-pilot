ab28f8cc16d399825956850be506866c68226ec1
Justin Stockton <jstockton@devis.com>
2015-01-06 15:36:52 -0700

    Add icons for completed steps and improve the logic for what can/should be clickable/focusable

--- app/scripts/directives/wizardNav.js
+++ app/scripts/directives/wizardNav.js
@@ -9,12 +9,29 @@
  */
 module.exports = function (Wizard) {
 
+    function modifyStepsForDisplay (steps) {
+        for (var i=0; i < steps.length; i++) {
+            steps[i].stepClass = steps[i].status;
+
+            if (steps[i].isComplete()) {
+                steps[i].stepClass += ' focusable';
+                steps[i].iconClass = 'icon cf-icon cf-icon-approved';
+                steps[i].iconText = '';
+            } else {
+                steps[i].iconClass = 'icon icon-num';
+                steps[i].iconText = i + 1;
+            }
+        }
+        return steps;
+    }
+
     return {
         restrict: 'E',
         link: function(scope) {
             scope.$on('$routeChangeSuccess', function (event, current, previous) {
                 if (current !== previous) {
-                    scope.steps = Wizard.getSteps();
+                    var steps = Wizard.getSteps();
+                    scope.steps = modifyStepsForDisplay(steps);
                 }
             });
         },
