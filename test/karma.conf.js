9d90839190b2182002b2f308f2492bd9ba1be8fa
Justin Stockton <jstockton@devis.com>
2015-01-10 15:18:15 -0700

    Include jQuery when running tests so that we have more selectors to choose from when testing directives

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/jquery/dist/jquery.js',
       'app/bundle/bundle.js',
       //'test/mock/**/*.js',
       'test/spec/**/*.js',
