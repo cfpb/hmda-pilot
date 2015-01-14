83ee39e559aa44c2c8d84fc95e75d8893c74c346
Justin Stockton <jstockton@devis.com>
2015-01-14 09:16:14 -0700

    Improve the comments in the ruleEngine service

--- app/scripts/services/ruleEngine.js
+++ app/scripts/services/ruleEngine.js
@@ -9,7 +9,12 @@
  */
 module.exports = function () {
 
-    // Get a list of the Fiscal Years in use by the HMDA Rule Engine
+    /**
+     * Get a list of the Fiscal Years in use by the HMDA Rule Engine
+     * TODO: Expose the get years method in the hmda-spec to the hmda-rule-engine
+     *
+     * @return {Array} fiscal years
+     */
     this.getFiscalYears = function() {
         return ['2014', '2013'];
     };
