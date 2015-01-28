9edc9fb1c3744ad475aedc736b846099fa79d93d
Donald Oellerich <doelleri@gmail.com>
2015-01-28 14:23:40 -0500

    update test for header depending on scope

--- test/spec/directives/errorDetail.js
+++ test/spec/directives/errorDetail.js
@@ -11,7 +11,8 @@ describe('Directive: ErrorDetail', function () {
 
     var element,
         scope,
-        mockErrors = {"S270": {"scope": "ts", "explanation": "Century and/or year for action taken date does not match activity century/year.", "description": "Century (CC) and Year (YY) of action taken date must = activity century/year (CCYY) for period being processed.", "errors": [{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} },{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} }], "action": "Correct Entry and Resubmit"}}; //jshint ignore:line
+        mockErrors = {"S270": {"scope": "ts", "explanation": "Century and/or year for action taken date does not match activity century/year.", "description": "Century (CC) and Year (YY) of action taken date must = activity century/year (CCYY) for period being processed.", "errors": [{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} },{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} }], "action": "Correct Entry and Resubmit"}, //jshint ignore:line
+            "S271": {"scope": "lar", "explanation": "Century and/or year for action taken date does not match activity century/year.", "description": "Century (CC) and Year (YY) of action taken date must = activity century/year (CCYY) for period being processed.", "errors": [{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} },{"lineNumber": "1", "properties": {"actionDate": "2013", "transmittalSheet.activityYear": "2012"} }], "action": "Correct Entry and Resubmit"}}; //jshint ignore:line
 
     beforeEach(inject(function($templateCache) {
         var directiveTemplate = null;
@@ -45,8 +46,30 @@ describe('Directive: ErrorDetail', function () {
         });
 
         describe('table', function() {
-            it('should display column headers', function() {
+            it('should display column headers including Loan/Application Number when scope is \'lar\'', inject(function ($rootScope, $compile) {
+                scope = $rootScope.$new();
+                scope.errorId = "S271";
+                scope.error = mockErrors[scope.errorId];
+                element = angular.element('<error-detail error="error"></error-detail>');
+                element = $compile(element)(scope);
+                scope.$digest();
+                table = jQuery('table', element);
+
                 expect(jQuery('thead th:eq(0)', table).text()).toBe('Loan/Application Number');
+                expect(jQuery('thead th:eq(0)', table).attr('scope')).toBe('col');
+
+                expect(jQuery('thead th:eq(1)', table).text()).toBe('Field');
+                expect(jQuery('thead th:eq(1)', table).attr('scope')).toBe('col');
+
+                expect(jQuery('thead th:eq(2)', table).text()).toBe('Submitted Value');
+                expect(jQuery('thead th:eq(2)', table).attr('scope')).toBe('col');
+
+                expect(jQuery('thead th:eq(3)', table).text()).toBe('Action');
+                expect(jQuery('thead th:eq(3)', table).attr('scope')).toBe('col');
+            }));
+
+            it('should display column headers including Line Number when scope is not \'lar\'', function() {
+                expect(jQuery('thead th:eq(0)', table).text()).toBe('Line Number');
                 expect(jQuery('thead th:eq(0)', table).attr('scope')).toBe('col');
 
                 expect(jQuery('thead th:eq(1)', table).text()).toBe('Field');
