93288b24e092988f795f2a168ffe2a7db80bddd8
Justin Stockton <jstockton@devis.com>
2015-01-29 10:37:32 -0700

    Reformat index.js to make it easier to maintain. Now it matches the directives and services.

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,19 +1,11 @@
 'use strict';
 
 require('angular');
+
 var app = angular.module('hmdaPilotApp');
 
-var SelectFileCtrl = require('./selectFile');
-app.controller('SelectFileCtrl', SelectFileCtrl);
-
-var SummarySyntacticalValidityCtrl = require('./summarySyntacticalValidity');
-app.controller('SummarySyntacticalValidityCtrl', SummarySyntacticalValidityCtrl);
-
-var SummaryQualityMacroCtrlCtrl = require('./summaryQualityMacro');
-app.controller('SummaryQualityMacroCtrl', SummaryQualityMacroCtrlCtrl);
-
-var SummaryMSAIRSCtrl = require('./summaryMSA-IRS');
-app.controller('SummaryMSAIRSCtrl', SummaryMSAIRSCtrl);
-
-var SubmitCtrl = require('./submit');
-app.controller('SubmitCtrl', SubmitCtrl);
+app.controller('SelectFileCtrl', require('./selectFile'));
+app.controller('SummarySyntacticalValidityCtrl', require('./summarySyntacticalValidity'));
+app.controller('SummaryQualityMacroCtrl', require('./summaryQualityMacro'));
+app.controller('SummaryMSAIRSCtrl', require('./summaryMSA-IRS'));
+app.controller('SubmitCtrl', require('./submit'));
