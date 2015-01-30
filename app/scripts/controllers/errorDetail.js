719000834bdd780215d2286daab21770a2ec680c
Justin Stockton <jstockton@devis.com>
2015-01-30 12:07:45 -0700

    Display a dropdown so that the user can navigate from one error detail directly to another

--- app/scripts/controllers/errorDetail.js
+++ app/scripts/controllers/errorDetail.js
@@ -16,9 +16,11 @@ module.exports = /*@ngInject*/ function ($scope, $routeParams, $location, HMDAEn
 
     $scope.editType = editType;
     $scope.editId = editId;
+    $scope.siblingEdits = [];
 
     if (editErrors[editType] && editErrors[editType][editId]) {
         $scope.editError = editErrors[editType][editId];
+        $scope.siblingEdits = Object.keys(editErrors[editType]);
     } else {
         $scope.editError = {};
     }
@@ -26,6 +28,12 @@ module.exports = /*@ngInject*/ function ($scope, $routeParams, $location, HMDAEn
     $scope.backToSummary = function() {
         if (editType === 'syntactical' || editType === 'validity') {
             $location.path('/summarySyntacticalValidity');
+        } else { // Go back to the start if nothing matches
+            $location.path('/');
         }
+    };
+
+    $scope.goToEditDetail = function() {
+        $location.path('/detail/' + editType + '/' + $scope.selectedEditId);
     };
 };
