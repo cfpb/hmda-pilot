2ce5974761c171fb3151082b45847a00c5b65f3d
Justin Stockton <jstockton@devis.com>
2015-02-03 10:56:50 -0700

    Display any quality or macro errors on the summary view

--- test/spec/controllers/summaryQualityMacro.js
+++ test/spec/controllers/summaryQualityMacro.js
@@ -6,22 +6,45 @@ require('angular-mocks');
 describe('Controller: SummaryQualityMacroCtrl', function () {
 
     var scope,
-        location;
+        location,
+        controller,
+        Wizard,
+        Session,
+        mockEngine,
+        mockErrors = {
+            quality: {},
+            macro: {}
+        };
 
     beforeEach(angular.mock.module('hmdaPilotApp'));
 
-    beforeEach(inject(function ($rootScope, $location, $controller) {
+    beforeEach(inject(function ($rootScope, $location, $controller, _Wizard_, _Session_) {
         scope = $rootScope.$new();
         location = $location;
-
+        controller = $controller;
+        Wizard = _Wizard_;
+        Session = _Session_;
+        mockEngine = {
+            getErrors: function() {
+                return mockErrors;
+            }
+        };
+        Wizard.initSteps();
         $controller('SummaryQualityMacroCtrl', {
             $scope: scope,
-            $location: location
+            $location: location,
+            Session : _Session_,
+            HMDAEngine: mockEngine,
+            Wizard: _Wizard_
         });
     }));
 
-    describe('previous()', function() {
-        // TODO: Stubbing out for now
+    it('should include the quality errors in the scope', function () {
+        expect(scope.qualityErrors).toEqual({});
+    });
+
+    it('should include the macro errors in the scope', function () {
+        expect(scope.macroErrors).toEqual({});
     });
 
     describe('hasNext()', function() {
@@ -29,6 +52,30 @@ describe('Controller: SummaryQualityMacroCtrl', function () {
     });
 
     describe('next()', function() {
-        // TODO: Stubbing out for now
+        beforeEach(function() {
+            scope.next();
+            scope.$digest();
+        });
+
+        it('should mark the current step in the wizard as complete', function () {
+            var steps = Wizard.getSteps();
+            expect(steps[0].isActive).toBeFalsy();
+            expect(steps[0].status).toBe('complete');
+        });
+
+        it('should direct the user to the /summaryMSA-IRS page', function () {
+            expect(location.path()).toBe('/summaryMSA-IRS');
+        });
+    });
+
+    describe('previous()', function () {
+        beforeEach(function() {
+            scope.previous();
+            scope.$digest();
+        });
+
+        it('should direct the user to the /summarySyntacticalValidity page', function () {
+            expect(location.path()).toBe('/summarySyntacticalValidity');
+        });
     });
 });
