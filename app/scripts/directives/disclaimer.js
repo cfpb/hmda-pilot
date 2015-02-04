639c303e13e89bbdf4a35aeb8a54327792bd61a8
Justin Stockton <jstockton@devis.com>
2015-02-03 17:06:01 -0700

    Add a disclaimer directive that displays and manages the state of a disclaimer shown at the top of each page.

---
+++ app/scripts/directives/disclaimer.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name hmdaPilotApp.directive:Disclaimer
+ * @description
+ * # Disclaimer directive
+ * Directive for displaying a collapsible disclaimer
+ */
+module.exports = /*@ngInject*/ function () {
+
+    var hideClass = 'u-visually-hidden';
+
+    function controller($scope, $element) {
+        $scope.toggle = function() {
+            // Hide the content
+            $element.toggleClass('collapsed');
+            $element.find('p').toggleClass(hideClass);
+
+            // Update the button
+            if ($element.find('p').hasClass(hideClass)) {
+                $element.find('button').text('more info ').append('<span class="cf-icon cf-icon-down"></span>');
+            } else {
+                $element.find('button').text('collapse ').append('<span class="cf-icon cf-icon-up"></span>');
+            }
+        };
+    }
+
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'partials/disclaimer.html',
+        controller: /*@ngInject*/ controller
+    };
+};
