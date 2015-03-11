29e3429cac7335f47b2cdf1ec2341a3def53f53a
Justin Stockton <jstockton@devis.com>
2015-03-11 16:27:30 -0600

    Don't rerun the special validations, if there are already special edits, when the user clicks the next button.

--- app/scripts/controllers/summaryQualityMacro.js
+++ app/scripts/controllers/summaryQualityMacro.js
@@ -27,6 +27,10 @@ module.exports = /*@ngInject*/ function ($scope, $location, $q, $timeout, HMDAEn
         return diff.length > 0;
     }
 
+    function hasErrors(obj) {
+        return Object.keys(obj).length > 0;
+    }
+
     // Populate the $scope
     $scope.errors = {};
     $scope.isProcessing = false;
@@ -46,10 +50,15 @@ module.exports = /*@ngInject*/ function ($scope, $location, $q, $timeout, HMDAEn
     };
 
     $scope.next = function() {
-        // Toggle processing flag on so that we can notify the user
-        $scope.isProcessing = true;
+        if (hasErrors(editErrors.special)) {
+            $location.path('/summaryMSA-IRS');
+        } else {
+            // Toggle processing flag on so that we can notify the user
+            $scope.isProcessing = true;
 
-        $timeout(function() { $scope.process(); }, 100); // Pause before starting the validation so that the DOM can update
+            // Pause before starting the validation so that the DOM can update
+            $timeout(function() { $scope.process(); }, 100);
+        }
     };
 
     $scope.process = function() {
