5ddcb555c02c9de4d7eef21cfc3b46828d131390
Justin Stockton <jstockton@devis.com>
2015-03-24 07:59:12 -0600

    Add a configuration file and Grunt task for production builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,7 +129,7 @@ module.exports = function (grunt) {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish'),
         ignores: [
-            '<%= yeoman.app %>/config/config.js'    
+            '<%= yeoman.app %>/config/config.js'
         ]
       },
       all: {
@@ -474,6 +474,19 @@ module.exports = function (grunt) {
         options: {
           patterns: [{
             json: grunt.file.readJSON('./config/environments/development.json')
+          }]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['./config/config.js'],
+          dest: '<%= yeoman.app %>/scripts/modules/'
+        }]
+      },
+      production: {
+        options: {
+          patterns: [{
+            json: grunt.file.readJSON('./config/environments/production.json')
           }]
         },
         files: [{
