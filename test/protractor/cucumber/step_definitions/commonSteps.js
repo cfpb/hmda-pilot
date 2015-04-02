5967e2f80352ef3b4c9ed8fac5b1df284d24494b
Henry Grover <henry@hgrover.com>
2015-04-02 12:26:16 -0400

    Added code to prevent onbeforeunload event from opening a troublesome dialog

--- test/protractor/cucumber/step_definitions/commonSteps.js
+++ test/protractor/cucumber/step_definitions/commonSteps.js
@@ -8,6 +8,8 @@ module.exports = function() {
 
     this.Given(/^that I am at the HMDA homepage$/, function(next) {
         browser.get('http://dev.hmda-pilot.ec2.devis.com/#/');
+        //Prevents "are you sure you want to leave?" window from popping up
+        browser.executeScript("window.onbeforeunload = function(){};");
         next();
     });
 };
