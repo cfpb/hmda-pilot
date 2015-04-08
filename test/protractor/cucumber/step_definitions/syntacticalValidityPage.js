9b6c30fafa8ff248bb5a90ad9cf3761e2610cf49
Henry Grover <henry@hgrover.com>
2015-04-08 18:50:15 -0400

    Added code for testing that syntactical/validity summaries exist

---
+++ test/protractor/cucumber/step_definitions/syntacticalValidityPage.js
@@ -0,0 +1,26 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+var expect = chai.expect;
+
+module.exports = function() {
+
+    this.When(/^I wait for the file to be processed$/, function (next) {
+        //Waits for URL to include "Syntactical"
+        //TODO: Set to wait for URL to change at all. More flexible.
+        browser.wait(function() {
+            return browser.getCurrentUrl().then(function(url) {
+                return (url.indexOf("Syntactical") !== -1);
+            });
+        }, 20000);
+
+        next();
+    });
+
+
+    this.Then(/^I see an electronic report showing how many syntactical\/validity edits failed$/, function (next) {
+        expect(element(by.id("syntactical")).isPresent()).to.eventually.be.true.notify(next);
+    });
+
+};
