314c6f44b56c4ff365d6adb082f15cc3600b0996
Henry Grover <henry@hgrover.com>
2015-04-08 19:23:50 -0400

    Moved file uploading into a named function for future flexibility

--- test/protractor/cucumber/step_definitions/uploadPage.js
+++ test/protractor/cucumber/step_definitions/uploadPage.js
@@ -10,18 +10,22 @@ var path = require('path');
 //Defining page elements
 var pageErrors = element.all(by.css(".page-errors"));
 var submitButton = element(by.css(".form-buttons button"));
-var fileSelector = element(by.id('file'));
 
 module.exports = function() {
 
-    this.When(/^I upload the "([^"]*)" file for validation$/, function (fileName, next) {
+    selectFile = function(fileName){
+        var fileSelector = element(by.id('file'));
+
         //Get filename as argument, convert into path, send path to selector on site.
         var fileToUpload = '../files/'+fileName;
         var absolutePath = path.resolve(__dirname, fileToUpload);
 
-        fileSelector.sendKeys(absolutePath).then(function(){
-            next();
-        });
+        fileSelector.sendKeys(absolutePath);
+    }
+
+    this.When(/^I upload the "([^"]*)" file for validation$/, function (fileName, next) {
+        selectFile(fileName);
+        next();
     });
 
     this.When(/^I click the submit button$/, function (next) {
