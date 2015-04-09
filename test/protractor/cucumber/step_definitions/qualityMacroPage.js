2172993fc63af9665fb5c074ed721f940aae05e2
Henry Grover <henry@hgrover.com>
2015-04-08 23:12:22 -0400

    Added tests for existence of quality/macro error tables

---
+++ test/protractor/cucumber/step_definitions/qualityMacroPage.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+var expect = chai.expect;
+
+module.exports = function() {
+
+    this.Then(/^I see an electronic report showing how many quality edits failed$/, function (next) {
+        expect(element(by.id("quality")).isPresent()).to.eventually.be.true.notify(next);
+    });
+
+    this.Then(/^I see an electronic report showing how many macro edits failed$/, function (next) {
+        expect(element(by.id("macro")).isPresent()).to.eventually.be.true.notify(next);
+    });
+
+};
