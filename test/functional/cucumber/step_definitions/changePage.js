18d796a1b730d5948304fdf9d2376ccf85b9700e
Henry Grover <henry@hgrover.com>
2015-04-14 09:30:19 -0400

    Make sure that page changes wait for the progress bar to disappear before continuing

--- test/functional/cucumber/step_definitions/changePage.js
+++ test/functional/cucumber/step_definitions/changePage.js
@@ -7,6 +7,7 @@ var expect = chai.expect;
 module.exports = function() {
 
     continueButton = element(by.buttonText("Continue"));
+    progressBar = element.all(by.css('div.ngdialog-overlay'));
 
     waitUrlChange = function(startUrl){
         //Waits for URL to change before allowing execution to move forward. Timeout is at end of fn.
@@ -23,12 +24,19 @@ module.exports = function() {
         else{
             oldUrl = startUrl;
         }
-        //Fulfill and return promise when URL changes
+        //Wait until URL changes
         browser.wait(function() {
             return browser.getCurrentUrl().then(function(url) {
                 return (url !== oldUrl);
             });
         }, 2000000);
+        //Wait until progress bar is closed
+        browser.wait(function() {
+            return progressBar.count().then(function(count) {
+                return (count === 0);
+            });
+        }, 2000000);
+        //Fulfill and return promise when URL changes and no progress bar exists
         deferred.fulfill();
         return deferred.promise;
     }
