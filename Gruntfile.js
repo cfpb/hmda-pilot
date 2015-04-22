dab255627040044a3fa91ac1ade4df2a30475789
Justin Stockton <jstockton@devis.com>
2015-04-22 15:09:23 -0600

    Cleanup function test config so that it properly runs and reports the tests nows from a grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -520,8 +520,6 @@ module.exports = function (grunt) {
     protractor: {
         options: {
             configFile: 'test/functional/conf.js', // Default config file
-            keepAlive: true, // If false, the grunt process stops when the test fails.
-            noColor: false, // If true, protractor will not use colors in its output.
             args: {
                 // Arguments passed to the command
             }
