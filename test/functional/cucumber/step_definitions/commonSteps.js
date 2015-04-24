d7e2221203de3f69a952ae02562d8e6556ae1087
Henry Grover <henry@hgrover.com>
2015-04-24 11:56:34 -0400

    Set up tests to log in on first getting to the site

--- test/functional/cucumber/step_definitions/commonSteps.js
+++ test/functional/cucumber/step_definitions/commonSteps.js
@@ -9,11 +9,20 @@ module.exports = function() {
     disclaimer = element(by.css('div.disclaimer'));
 
     this.Given(/^that I am at the HMDA homepage$/, function(next) {
+        passwordBox = element.all(by.id('txt-pwd'));
+        loginButton = element.all(by.css('.login-button'))
+
         browser.get('http://dev.hmda-pilot.ec2.devis.com/#/');
+
         //Prevents 'are you sure you want to leave?' window from popping up
         browser.executeScript('window.onbeforeunload = function(){};').then(function(){
-            next();
+            if(passwordBox.count() !== 0){
+                //Log in if we have not already done so
+                passwordBox.sendKeys('p1l0t');
+                loginButton.click();
+            }
         });
+            next();
     });
 
     this.Then(/^I will see a disclaimer at the top$/, function (next) {
