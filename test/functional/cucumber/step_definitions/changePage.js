77dd25797e4b31d5c27534a9415b070288d99cd7
David Schauder <porterbot@github.com>
2015-04-27 07:08:26 -0400

    cleaned up callback/promises and reset back to dev

--- test/functional/cucumber/step_definitions/changePage.js
+++ test/functional/cucumber/step_definitions/changePage.js
@@ -24,36 +24,32 @@ module.exports = function() {
         });
     };
 
-    waitUrlChange = function(startUrl){
+    waitUrlChange = function(oldUrl) {
         //Waits for URL to change before allowing execution to move forward. Timeout is at end of fn.
         //Finding start URL within fn is slow, and can happen after a quick page change has occurred
         //As such, you can pass a start URL (as string) to it and that will be used.
         var deferred = protractor.promise.defer();
         //If URL wasn't passed, find one here. Can lead to timeouts with a quick page change
-        if(typeof startUrl === 'undefined'){
+        if (!oldUrl) {
             browser.getCurrentUrl().then(function(url){
                 oldUrl = url;
             });
         }
-        //Otherwise, take passed URL to compare
-        else{
-            oldUrl = startUrl;
-        }
-        //Wait until URL changes
-        browser.wait(function() {
-            return browser.getCurrentUrl().then(function(url) {
-                return (url !== oldUrl);
-            });
-        }, 2000000);
-        //Wait until progress bar is closed
-        browser.wait(function() {
-            return progressBar.count().then(function(count) {
-                return (count === 0);
-            });
-        }, 2000000);
         //Fulfill and return promise when URL changes and no progress bar exists
-        deferred.fulfill();
-        return deferred.promise;
+        var pageChangeConditions = [
+            browser.wait(function() {
+                return browser.getCurrentUrl().then(function(url) {
+                    return (url !== oldUrl);
+                });
+            }, 2000000),
+            browser.wait(function() {
+                return progressBar.count().then(function(count) {
+                    return (count === 0);
+                });
+            }, 2000000)
+        ];
+        
+        return protractor.promise.all(pageChangeConditions);
     }
 
     this.When(/^I wait for the file to be processed$/, function (next) {
@@ -79,19 +75,13 @@ module.exports = function() {
 
     this.When(/^I continue to the msa and irs edit reports page$/, function (next) {
     var recentlyChangedUrl;
-        waitUrlChange().then(function(){
-            browser.getCurrentUrl().then(function(url){
-                recentlyChangedUrl = url
-            }).then(function(){
+        waitUrlChange().then(function() {
+            continueButton.click();
+            waitUrlChange().then(function() {
                 continueButton.click();
-            }).then(function(){
-                waitUrlChange().then(function(){
+                waitUrlChange().then(function() {
                     continueButton.click();
-                }).then(function(){
-                    waitUrlChange().then(function(){
-                        continueButton.click();
-                        next();
-                    });
+                    next();
                 });
             });
         });
@@ -99,24 +89,17 @@ module.exports = function() {
 
     this.When(/^I continue through the quality macro errors page$/, function (next) {
         var recentlyChangedUrl;
-        waitUrlChange().then(function(){
-            browser.getCurrentUrl().then(function(url){
-                recentlyChangedUrl = url
-            }).then(function(){
-                continueButton.click();
-            }).then(function(){
-                waitUrlChange().then(function() {
-                    element.all(by.partialLinkText('Q0')).then(function (macroErrors) {
-                        macroErrors[0].click().then(function() {
-                            verifyMacroErrors (0, macroErrors.length).then(function () {
-                                continueButton.click().then(function() {
-                                    waitUrlChange().then(function() {
-                                        next();
-                                    });
-                                });
-                            });
-                        })
-                    })
+        waitUrlChange().then(function() {
+            continueButton.click();
+            waitUrlChange().then(function() {
+                element.all(by.partialLinkText('Q0')).then(function(macroErrors) {
+                    macroErrors[0].click();
+                    verifyMacroErrors (0, macroErrors.length).then(function() {
+                        continueButton.click();
+                        waitUrlChange().then(function() {
+                            next();
+                        });
+                    });
                 });
             });
         });
