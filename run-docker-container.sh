6ecdfde0324a18707894fde224f65447a50e76ac
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-09-23 15:53:29 -0700

    Cleanup old scripts

--- run-docker-container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Remove any current container for the UI
-docker rm -f hmda-pilot
-# Run the UI container, linking in the api container
-docker run -d --link hmda-pilot-api --name hmda-pilot -p 80:80 hmda-pilot
