f5fefa6a43ce9e255e11bb0d73bdbed4e5e017c8
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-09-23 23:27:48 -0700

    Remove imagemin grunt task which fails on alpine

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,17 +289,16 @@ module.exports = function (grunt) {
     //   dist: {}
     // },
 
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-
+//    imagemin: {
+//      dist: {
+//        files: [{
+//          expand: true,
+//          cwd: '<%= yeoman.app %>/images',
+//          src: '{,*/}*.{png,jpg,jpeg,gif}',
+//          dest: '<%= yeoman.dist %>/images'
+//        }]
+//      }
+//    },
     svgmin: {
       dist: {
         files: [{
@@ -383,7 +382,7 @@ module.exports = function (grunt) {
       ],
       dist: [
         'copy:styles',
-        'imagemin',
+       // 'imagemin',
         'svgmin'
       ]
     },
